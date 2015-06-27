17c5d0b52da89be5a94f5735e7aab92ed308ed34
danieljin <danielkjin@gmail.com>
2015-06-27 19:31:35 -0400

    make logout work

--- assets/main.js
+++ assets/main.js
@@ -6,12 +6,20 @@ $(function() {
 
     if( $(this).filter('.disabled').size() === 0) {
       $('.' + direction + '.sidebar').not('.styled').sidebar('setting', {
-          dimPage          : dimPage,
-          transition       : transition,
-          mobileTransition : transition
-        })
-      ;
+        dimPage          : dimPage,
+        transition       : transition,
+        mobileTransition : transition
+      });
+
       $('.' + direction + '.sidebar').not('.styled').sidebar('toggle');
     }
+  });
+
+  $('#logout').click(function() {
+    localStorage.removeItem("user_id");
+    localStorage.removeItem("role");
+    localStorage.removeItem("building_id");
+
+    window.location.replace('index.html');
   });
 });
